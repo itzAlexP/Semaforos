f18a1ab92484c5f314710c11982f1d7924211aee
itzAlexP <aperezbcn94@gmail.com>
2017-11-07 01:18:21 -0800

    Depurado codigo

--- main.cpp
+++ main.cpp
@@ -25,6 +25,11 @@
 #define RADIO_AVATAR 25.f
 #define OFFSET_AVATAR 5
 
+#define ITERACIONES_TOTALES 20
+#define POSICION_X_TREN_ROJO 2.f
+#define POSICION_Y_TREN_AZUL 2.f
+#define POSICION_Y_TREN_VERDE 6.f
+
 pid_t
 pidTrenRojo,
 pidTrenAzul,
@@ -50,13 +55,10 @@ struct posicionesTrenes
 
 union semun
 {
-
     int val;
     struct semid_ds* buf;
     unsigned short* array;
 };
-
-
 
 //Transformar posiciones del 0 a 7 a pixeles de 0 a 512 para colocar las piezas.
 sf::Vector2f BoardToWindows(sf::Vector2f _position)
@@ -64,106 +66,98 @@ sf::Vector2f BoardToWindows(sf::Vector2f _position)
     return sf::Vector2f(_position.x*LADO_CASILLA+OFFSET_AVATAR, _position.y*LADO_CASILLA+OFFSET_AVATAR);
 }
 
+//Cuando un tren ha finalizado las iteraciones avisa al padre
 void finalizadoTren(int param)
 {
-
     trenesFinalizados ++;
-
 }
 
-//Se mueve el tren Verde
-
-void TrenVerde(struct posicionesTrenes* posiciones, int semId){
-
+//Se mueve el tren verde
+void TrenVerde(struct posicionesTrenes* posiciones, int semId)
+{
+    //Creamos el buff del semaforo
     struct sembuf sb[1];
     sb[0].sem_flg = SEM_UNDO;
 
     int iteraciones = 0;
 
+    //Inicializamos las posiciones iniciales del tren verde
     posiciones -> posicionesTrenVerde[0] = 5;
     posiciones -> posicionesTrenVerde[1] = 6;
     posiciones -> posicionesTrenVerde[2] = 7;
 
     bool finalizado = false;
 
+    //Mientras no realice las 20 iteraciones se ira moviendo
     while(!finalizado)
     {
-
         //Zona critica, activamos semaforo
-        if(posiciones -> posicionesTrenVerde[0] - 1 == 2)
+        if(posiciones -> posicionesTrenVerde[0] - 1 == POSICION_X_TREN_ROJO)
         {
-
             sb[0].sem_num = 1;
             sb[0].sem_op = -1;
             semop(semId, sb, 1);
         }
 
-
+        //Movemos las piezas del tren, si es la ultima iteracion desapareceran las piezas
         for(int i = 0; i < 3; i++)
         {
-
-            if(iteraciones == 1 && posiciones -> posicionesTrenVerde[i] <= 0)
+            //Ultima iteracion
+            if(iteraciones == ITERACIONES_TOTALES - 1 && posiciones -> posicionesTrenVerde[i] <= 0)
             {
                 posiciones -> posicionesTrenVerde[i] = -1;
-
+                //Ha acabado de moverse la ultima pieza en la ultima iteracion
                 if(posiciones -> posicionesTrenVerde[2] == -1)
                 {
                     finalizado = true;
                 }
-
             }
+            //Si no estamos en la ultima iteracion
             else
             {
+                //Si las piezas no han llegado al final del tablero
                 if(posiciones -> posicionesTrenVerde[i] - 1 >= 0)
                 {
                     posiciones -> posicionesTrenVerde[i] -= 1;
-
-
                 }
+
+                //Si las piezas han llegado al final del tablero vuelven al otro lado
                 else
                 {
                     posiciones -> posicionesTrenVerde[i] = 7;
+                    //Cuando la ultima pieza llega al otro lado del tablero incrementamos las iteraciones
                     if(posiciones -> posicionesTrenVerde[2] == 7 && i == 2)
                     {
                         iteraciones++;
-
                     }
                 }
             }
-
-
         }
-
-
         //Salimos zona critica
         if(posiciones -> posicionesTrenVerde[2] == 1)
         {
-
             sb[0].sem_num = 1;
             sb[0].sem_op = 1;
             semop(semId, sb, 1);
-
         }
+        //Paramos el tren
         sleep(1);
-
     }
-
+    //Al acabar liberamos memoria y avisamos el padre de que el proceso va a finalizar.
     shmdt(posiciones);
     kill(getppid(), SIGUSR1);
-
-
-
 }
 
 //Se mueve el tren Rojo
 void TrenRojo(struct posicionesTrenes* posiciones, int semId)
 {
+    //Creamos el buff del semaforo
     struct sembuf sb[1];
     sb[0].sem_flg = SEM_UNDO;
 
-
     int iteraciones = 0;
 
+    //Inicializamos las posiciones iniciales del tren rojo
     posiciones -> posicionesTrenRojo[0] = 2;
     posiciones -> posicionesTrenRojo[1] = 3;
     posiciones -> posicionesTrenRojo[2] = 4;
@@ -172,55 +166,51 @@ void TrenRojo(struct posicionesTrenes* posiciones, int semId)
     posiciones -> posicionesTrenRojo[5] = 7;
 
     bool finalizado = false;
-
+    //Mientras no realice las 20 iteraciones se ira moviendo
     while(!finalizado)
     {
-
-        //Zona critica, activamos semaforo
-        if(posiciones -> posicionesTrenRojo[0] - 1 == 2)
+        //Zona critica con el tren azul, activamos semaforo
+        if(posiciones -> posicionesTrenRojo[0] - 1 == POSICION_Y_TREN_AZUL)
         {
-
             sb[0].sem_num = 0;
             sb[0].sem_op = -1;
             semop(semId, sb, 1);
         }
-
-
-        if(posiciones -> posicionesTrenRojo[0] == 6)
+        //Zona critica con el tren verde, activamos semaforo
+        if(posiciones -> posicionesTrenRojo[0] == POSICION_Y_TREN_VERDE)
         {
-
             sb[0].sem_num = 1;
             sb[0].sem_op = -1;
             semop(semId, sb, 1);
         }
-
+        //Movemos las piezas del tren, si es la ultima iteracion desapareceran las piezas
         for(int i = 0; i < 6; i++)
         {
-
-
-            if(iteraciones == 1 && posiciones -> posicionesTrenRojo[i] <= 0)
+            //Ultima iteracion
+            if(iteraciones == ITERACIONES_TOTALES - 1 && posiciones -> posicionesTrenRojo[i] <= 0)
             {
                 posiciones -> posicionesTrenRojo[i] = -1;
 
-
-
+                //Ha acabado de moverse la ultima pieza en la ultima iteracion
                 if(posiciones -> posicionesTrenRojo[5] == -1)
                 {
                     finalizado = true;
                 }
-
             }
+            //Si no estamos en la ultima iteracion
             else
             {
+                //Si las piezas no han llegado al final del tablero
                 if(posiciones -> posicionesTrenRojo[i] - 1 >= 0)
                 {
                     posiciones -> posicionesTrenRojo[i] -= 1;
-
-
                 }
+                //Si las piezas han llegado al final del tablero vuelven al otro lado
                 else
                 {
                     posiciones -> posicionesTrenRojo[i] = 7;
+
+                    //Cuando la ultima pieza llega al otro lado del tablero incrementamos las iteraciones
                     if(posiciones -> posicionesTrenRojo[5] == 7 && i == 5)
                     {
                         iteraciones++;
@@ -228,46 +218,39 @@ void TrenRojo(struct posicionesTrenes* posiciones, int semId)
                     }
                 }
             }
-
-
         }
-
-        //Salimos de la zona de riesgo
-        if(posiciones -> posicionesTrenRojo[5] == 1)
+        //Salimos de la zona de riesgo con el tren azul
+        if(posiciones -> posicionesTrenRojo[5] == POSICION_Y_TREN_AZUL -1)
         {
-
             sb[0].sem_num = 0;
             sb[0].sem_op = 1;
             semop(semId, sb, 1);
-
         }
-
-        if(posiciones -> posicionesTrenRojo[5] == 5)
+        //Salimos de la zona de riesgo con el tren verde
+        if(posiciones -> posicionesTrenRojo[5] == POSICION_Y_TREN_VERDE -1)
         {
-
             sb[0].sem_num = 1;
             sb[0].sem_op = 1;
             semop(semId, sb, 1);
-
         }
-
+        //Paramos el tren
         sleep(2);
-
     }
+    //Al acabar liberamos memoria y avisamos el padre de que el proceso va a finalizar.
     shmdt(posiciones);
     kill(getppid(), SIGUSR1);
-
-
 }
 
 //Se mueve el tren Azul
 void TrenAzul(struct posicionesTrenes* posiciones, int semId)
 {
+    //Creamos el buff del semaforo
     struct sembuf sb[1];
     sb[0].sem_flg = SEM_UNDO;
 
     int iteraciones = 0;
 
+    //Inicializamos las posiciones iniciales del tren azul
     posiciones -> posicionesTrenAzul[0] = 4;
     posiciones -> posicionesTrenAzul[1] = 5;
     posiciones -> posicionesTrenAzul[2] = 6;
@@ -275,70 +258,67 @@ void TrenAzul(struct posicionesTrenes* posiciones, int semId)
 
     bool finalizado = false;
 
+    //Mientras no realice las 20 iteraciones se ira moviendo
     while(!finalizado)
     {
-
-        //Zona critica, activamos semaforo
-        if(posiciones -> posicionesTrenAzul[0] - 1 == 2)
+        //Zona critica con tren rojo, activamos semaforo
+        if(posiciones -> posicionesTrenAzul[0] - 1 == POSICION_X_TREN_ROJO)
         {
-
             sb[0].sem_num = 0;
             sb[0].sem_op = -1;
             semop(semId, sb, 1);
         }
-
-
+        //Movemos las piezas del tren, si es la ultima iteracion desapareceran las piezas
         for(int i = 0; i < 4; i++)
         {
-
-            if(iteraciones == 1 && posiciones -> posicionesTrenAzul[i] <= 0)
+            //Ultima iteracion
+            if(iteraciones == ITERACIONES_TOTALES - 1 && posiciones -> posicionesTrenAzul[i] <= 0)
             {
                 posiciones -> posicionesTrenAzul[i] = -1;
 
+                //Ha acabado de moverse la ultima pieza en la ultima iteracion
                 if(posiciones -> posicionesTrenAzul[3] == -1)
                 {
                     finalizado = true;
                 }
 
             }
+            //Si no estamos en la ultima iteracion
             else
             {
+                //Cuando la ultima pieza llega al otro lado del tablero incrementamos las iteraciones
                 if(posiciones -> posicionesTrenAzul[i] - 1 >= 0)
                 {
                     posiciones -> posicionesTrenAzul[i] -= 1;
-
-
                 }
+
+                //Si las piezas han llegado al final del tablero vuelven al otro lado
                 else
                 {
                     posiciones -> posicionesTrenAzul[i] = 7;
+
+                    //Cuando la ultima pieza llega al otro lado del tablero incrementamos las iteraciones
                     if(posiciones -> posicionesTrenAzul[3] == 7 && i == 3)
                     {
                         iteraciones++;
-
                     }
                 }
             }
-
-
         }
 
         //Salimos zona critica
         if(posiciones -> posicionesTrenAzul[3] == 1)
         {
-
             sb[0].sem_num = 0;
             sb[0].sem_op = 1;
             semop(semId, sb, 1);
-
         }
+        //Paramos el tren
         sleep(1);
-
     }
-
+    //Al acabar liberamos memoria y avisamos el padre de que el proceso va a finalizar.
     shmdt(posiciones);
     kill(getppid(), SIGUSR1);
-
 }
 
 //Pintamos por pantalla
@@ -349,10 +329,11 @@ void DibujarTrenes(struct posicionesTrenes* posiciones)
     sf::RenderWindow window(sf::VideoMode(512,512), "Carretera y manta");
     while(window.isOpen() && trenesFinalizados != 2)
     {
-        //Captura de eventos
+        //Captura de eventos, si no los capturamos crashea el programa
         sf::Event event;
         while (window.pollEvent(event)) {}
 
+        //Refrescamos pantalla
         window.clear();
 
         //Pintar tablero
@@ -391,25 +372,22 @@ void DibujarTrenes(struct posicionesTrenes* posiciones)
         for(int i = 0; i < 4; i++)
         {
 
-            sf::Vector2f posicionTrenAzul(posiciones->posicionesTrenAzul[i], 2.f);
+            sf::Vector2f posicionTrenAzul(posiciones->posicionesTrenAzul[i], POSICION_Y_TREN_AZUL);
             posicionTrenAzul = BoardToWindows(posicionTrenAzul);
             trenAzul.setPosition(posicionTrenAzul);
             window.draw(trenAzul);
 
         }
-
         //Pintamos vagones tren rojo
         sf::CircleShape trenRojo(RADIO_AVATAR);
         trenRojo.setFillColor(sf::Color::Red);
 
         for(int i = 0; i < 6; i++)
         {
-
-            sf::Vector2f posicionTrenRojo(2.f, posiciones->posicionesTrenRojo[i]);
+            sf::Vector2f posicionTrenRojo(POSICION_X_TREN_ROJO, posiciones->posicionesTrenRojo[i]);
             posicionTrenRojo = BoardToWindows(posicionTrenRojo);
             trenRojo.setPosition(posicionTrenRojo);
             window.draw(trenRojo);
-
         }
 
         //Pintamos vagones tren verde
@@ -418,28 +396,23 @@ void DibujarTrenes(struct posicionesTrenes* posiciones)
 
         for(int i = 0; i < 3; i++)
         {
-
-            sf::Vector2f posicionTrenVerde(posiciones->posicionesTrenVerde[i], 6.f);
+            sf::Vector2f posicionTrenVerde(posiciones->posicionesTrenVerde[i], POSICION_Y_TREN_VERDE);
             posicionTrenVerde = BoardToWindows(posicionTrenVerde);
             trenVerde.setPosition(posicionTrenVerde);
             window.draw(trenVerde);
-
         }
-
+        //Mostramos por pantalla
         window.display();
     }
-
 }
 
 int main()
 {
-
     quienSoy = TipoProceso::PADRE;
 
     //Reservamos memoria compartida
     int idSharedMemoryTrenes = shmget(IPC_PRIVATE, sizeof(struct posicionesTrenes), IPC_CREAT|0666);
     struct posicionesTrenes* ptrSharedMemoryTrenes = (struct posicionesTrenes*) shmat(idSharedMemoryTrenes,NULL, 0);
-
 
     //Inicializar semaforos
     int semId = semget(IPC_PRIVATE, 2, IPC_CREAT|0600);
@@ -448,8 +421,7 @@ int main()
     arg.val = 1;
     int okSem = semctl(semId, 0, SETALL, arg);
 
-
-    //Creamos proceso tren rojo y tren azul
+    //Creamos proceso tren azul
     pidTrenAzul = fork();
 
     //Tren azul
@@ -461,6 +433,7 @@ int main()
 
     else
     {
+        //Creamos proceso tren rojo
         pidTrenRojo = fork();
 
         //Tren rojo
@@ -471,36 +444,30 @@ int main()
 
         }
     }
-
-    //Padre
     if(quienSoy == TipoProceso::PADRE)
     {
-
+        //Creamos proceso tren verde
         pidTrenVerde = fork();
 
-        if(pidTrenVerde == 0){
-
+        //Tren verde
+        if(pidTrenVerde == 0)
+        {
             quienSoy = TipoProceso::VERDE;
             TrenVerde(ptrSharedMemoryTrenes, semId);
-        }else{
-             //Configuramos signals
-        signal(SIGUSR1, finalizadoTren);
-
-        //Pintar trenes
-        DibujarTrenes(ptrSharedMemoryTrenes);
-
-        //Liberamos memoria y semaforos.
-        shmdt(ptrSharedMemoryTrenes);
-        shmctl(ptrSharedMemoryTrenes, IPC_RMID, NULL);
-        semctl(semId, 0, IPC_RMID, arg);
-
-
         }
+        else
+        {
+            //Configuramos signals
+            signal(SIGUSR1, finalizadoTren);
 
+            //Pintar trenes
+            DibujarTrenes(ptrSharedMemoryTrenes);
 
-
-
+            //Liberamos memoria y semaforos.
+            shmdt(ptrSharedMemoryTrenes);
+            shmctl(ptrSharedMemoryTrenes, IPC_RMID, NULL);
+            semctl(semId, 0, IPC_RMID, arg);
+        }
     }
-
     return 0;
 }
